a68248ac83bef1ed102ae1f2c0d84b56e28cbbf7
Coen Konings <coen.konings@hotmail.com>
2023-09-09 16:03:20 +0200

    finish database design

--- rooster_import/admin.py
+++ rooster_import/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Track
 
-# Register your models here.
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
--- rooster_import/models.py
+++ rooster_import/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Track(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    tracks = models.ManyToManyField(Track)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    course = models.ForeignKey(Course)
+
+
 class Event(models.Model):
     description = models.CharField(max_length=255, null=False)
     location = models.charField(max_length=255, null=True)
