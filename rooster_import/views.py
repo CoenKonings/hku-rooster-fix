f4b5c6c1a8720ab58ee631fce1b83f5175746859
Coen Konings <coen.konings@hotmail.com>
2023-09-12 15:52:54 +0200

    display courses and groups in form

--- rooster_import/views.py
+++ rooster_import/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Track, Course
 
 
 class CreateFeedView(TemplateView):
-    template_name = "rooster_import/test.html"
+    template_name = "rooster_import/create-calendar-form.html"
+
+    def get(self, request):
+        tracks = Track.objects.all()
+        courses = Course.objects.all()
+
+        return render(request, self.template_name, {"tracks": tracks, "courses": courses})
