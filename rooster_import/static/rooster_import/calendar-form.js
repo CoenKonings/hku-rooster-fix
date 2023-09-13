c9bee42e19266016dd7f1aad8aa9b4013f8cd668
Coen Konings <coen.konings@hotmail.com>
2023-09-13 21:54:53 +0200

    course and group selection in javascript

--- rooster_import/static/rooster_import/calendar-form.js
+++ rooster_import/static/rooster_import/calendar-form.js
@@ -1,12 +1,16 @@
 {
     let tracks = [];
+    let courses = [];
+    let groups = [];
 
     Array.prototype.forEach.call(document.getElementsByClassName("track-selector"), trackSelector => {
         const updateCourses = () => {
             Array.prototype.forEach.call(document.getElementsByClassName("course-selector"), courseSelector => {
+                courseTracks = courseSelector.getAttribute("tracks")
+
                 if (tracks.length === 0) {
                     courseSelector.style.display = "block";
-                } else if (courseSelector.getAttribute("tracks").split(",").some(track => tracks.includes(track))) {
+                } else if (courseTracks.split(",").some(track => tracks.includes(track)) || courseTracks === "") {
                     courseSelector.style.display = "block";
                 } else {
                     courseSelector.style.display = "none";
@@ -22,6 +26,45 @@
             }
 
             updateCourses();
+        });
+    });
+
+    Array.prototype.forEach.call(document.getElementsByClassName("course-checkbox"), courseCheckbox => {
+        courseCheckbox.addEventListener("change", event => {
+            const courseId = event.target.id.split("-")[1];
+            const groupSelect = document.getElementById(`group-course-${courseId}`);
+
+            if (event.target.checked) {
+                courses.push(courseId);
+
+                if (groupSelect) {
+                    groupSelect.style.display = "inline";
+                }
+            } else {
+                courses = courses.filter(course => course !== courseId);
+
+                if (groupSelect) {
+                    groupSelect.style.display = "none";
+                }
+            }
+        });
+    });
+
+    Array.prototype.forEach.call(document.getElementsByClassName("group-select"), groupSelect => {
+        if (!courses.includes(groupSelect.id.split("-")[2])) {
+            groupSelect.style.display = "none";
+        }
+
+        groupSelect.addEventListener("change", event => {
+            const prevValue = event.target.getAttribute("prevValue");
+            if (prevValue) {
+                groups = groups.filter(group => group !== prevValue);
+            }
+
+            groups.push(event.target.value);
+            event.target.setAttribute("prevValue", event.target.value);
+
+            console.log(groups);
         });
     });
 }
