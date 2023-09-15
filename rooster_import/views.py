2835f37e1ea20fc24648f4d2dc4f57c6d04f07f9
Coen Konings <coen.konings@hotmail.com>
2023-09-15 16:40:51 +0200

    fix csrf issue

--- rooster_import/views.py
+++ rooster_import/views.py
@@ -3,12 +3,15 @@ from django.views.generic import TemplateView
 from .models import Track, Course
 from django.http import JsonResponse
 from .helpers import parse_request
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 
 
 class CreateFeedView(TemplateView):
     template_name = "rooster_import/create-calendar-form.html"
 
+    @ensure_csrf_cookie
     def get(self, request):
         tracks = Track.objects.all()
         courses = Course.objects.all()
